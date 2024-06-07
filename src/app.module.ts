b2f23aa3a91c2f279467dcf205964f486f41df49
marcopyre <marco.pyre@epsi.fr>
2024-06-07 16:13:39 +0200

    added docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { Product } from './commandes/commandes.entity';
-import { ProductService } from './commandes/commandes.service';
 import { AppController } from './app.controller';
 import { User } from './users/users.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,17 +6,19 @@ import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import * as dotenv from 'dotenv';
 import { ProductController } from './commandes/commandes.controller';
+import { ProductService } from './commandes/commandes.service';
+import { Product } from './commandes/commandes.entity';
 dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DBHOST,
+      host: 'db',
       port: 5432,
       username: process.env.DBUSER,
       password: process.env.DBPASS,
-      database: process.env.DBNAME,
+      database: 'db',
       entities: [Product, User],
       synchronize: true,
     }),
