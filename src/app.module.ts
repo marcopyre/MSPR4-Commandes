4416b3a618ca76373a7a74835c73567bceea551d
marcopyre <marco.pyre@epsi.fr>
2024-06-07 14:41:22 +0200

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { Product } from './commandes/commandes.entity';
 import { ProductService } from './commandes/commandes.service';
-import { ProductController } from './commandes/client.controller';
 import { AppController } from './app.controller';
 import { User } from './users/users.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import * as dotenv from 'dotenv';
+import { ProductController } from './commandes/commandes.controller';
 dotenv.config();
 
 @Module({
