41bf993037201cc1b496f54b50e281511e02ef78
marco <marco.pyre@epsi.fr>
2024-07-06 21:01:24 +0200

    build fix

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,25 +1,21 @@
 'use strict';
 
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { Resource } from '@opentelemetry/resources';
 import * as opentelemetry from '@opentelemetry/sdk-node';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 
 const exporterOptions = {
   url: 'http://localhost:4318/v1/traces',
 };
 
 const traceExporter = new OTLPTraceExporter(exporterOptions);
-
-// Configure the SDK to use the BatchSpanProcessor with the trace exporter
 const sdk = new opentelemetry.NodeSDK({
-  spanProcessor: new BatchSpanProcessor(traceExporter),
-  instrumentations: [new HttpInstrumentation(), new ExpressInstrumentation()],
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'orders-api',
+    [SemanticResourceAttributes.SERVICE_NAME]: 'clients-api',
   }),
 });
 
